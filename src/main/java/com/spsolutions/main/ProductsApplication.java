8e251214be23b9f7c1a39ee793e548c7d8966344
@sergio.trujillo <sergio.trujillo@outlook.com>
2020-10-23 19:52:59 -0500

    Se habilitan los repositorios de JPA en el proyecto

--- src/main/java/com/spsolutions/main/ProductsApplication.java
+++ src/main/java/com/spsolutions/main/ProductsApplication.java
@@ -3,9 +3,11 @@ package com.spsolutions.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.spsolutions.entity"})
+@EnableJpaRepositories(basePackages = {"com.spsolutions.repository"})
 public class ProductsApplication {
 
     public static void main(String[] args) {
