cb230de936baa008168aa83979c4aa0b7987f8b8
@sergio.trujillo <sergio.trujillo@outlook.com>
2020-10-23 20:17:13 -0500

    Se decora con la anotacion de servicio

--- src/main/java/com/spsolutions/service/impl/ProductServiceImpl.java
+++ src/main/java/com/spsolutions/service/impl/ProductServiceImpl.java
@@ -4,11 +4,13 @@ import com.spsolutions.entity.Product;
 import com.spsolutions.repository.ProductRepository;
 import com.spsolutions.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 @Transactional
 public class ProductServiceImpl implements ProductService {
 
